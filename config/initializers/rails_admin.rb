2f87e3cc698aad54d0f8c6a4254e676d1937d558
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-11-22 14:27:42 -0600

    better admin formatting, success message for survey

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -191,25 +191,7 @@ RailsAdmin.config do |config|
 
   ###  SurveySubmission  ###
 
-  # config.model 'SurveySubmission' do
-
-  #   # You can copy this to a 'rails_admin do ... end' block inside your survey_submission.rb model definition
-
-  #   # Found associations:
-
-  #     configure :user, :belongs_to_association 
-  #     configure :survey, :belongs_to_association 
-
-  #   # Found columns:
-
-  #     configure :id, :integer 
-  #     configure :user_id, :integer         # Hidden 
-  #     configure :survey_id, :integer         # Hidden 
-  #     configure :created_at, :datetime 
-  #     configure :updated_at, :datetime 
-
-  #   # Cross-section configuration:
-
+  config.model 'SurveySubmission' do
   #     # object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
   #     # label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
   #     # label_plural 'My models'      # Same, plural
@@ -217,9 +199,16 @@ RailsAdmin.config do |config|
   #     # parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
   #     # navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
 
-  #   # Section specific configuration:
-
-  #     list do
+    show do
+      field :user
+      field :survey
+      field :response_data do
+        pretty_value do
+          value.map {|k, v| "#{ k }: #{ v }" }.join ', '
+        end
+      end
+      field :created_at
+    end
   #       # filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
   #       # items_per_page 100    # Override default_items_per_page
   #       # sort_by :id           # Sort column (default is primary key)
@@ -228,15 +217,12 @@ RailsAdmin.config do |config|
   #     show do; end
   #     edit do; end
   #     export do; end
-  #     # also see the create, update, modal and nested sections, which override edit in specific cases (resp. when creating, updating, modifying from another model in a popup modal or modifying from another model nested form)
-  #     # you can override a cross-section field configuration in any section with the same syntax `configure :field_name do ... end`
-  #     # using `field` instead of `configure` will exclude all other fields and force the ordering
-  # end
+  end
 
 
   ###  User  ###
 
-  # config.model 'User' do
+  config.model 'User' do
 
   #   # You can copy this to a 'rails_admin do ... end' block inside your user.rb model definition
 
@@ -261,9 +247,7 @@ RailsAdmin.config do |config|
   #     configure :created_at, :datetime 
   #     configure :updated_at, :datetime 
 
-  #   # Cross-section configuration:
-
-  #     # object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
+    object_label_method :email     # Name of the method called for pretty printing an *instance* of ModelName
   #     # label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
   #     # label_plural 'My models'      # Same, plural
   #     # weight 0                      # Navigation priority. Bigger is higher.
@@ -284,6 +268,6 @@ RailsAdmin.config do |config|
   #     # also see the create, update, modal and nested sections, which override edit in specific cases (resp. when creating, updating, modifying from another model in a popup modal or modifying from another model nested form)
   #     # you can override a cross-section field configuration in any section with the same syntax `configure :field_name do ... end`
   #     # using `field` instead of `configure` will exclude all other fields and force the ordering
-  # end
+  end
 
 end
