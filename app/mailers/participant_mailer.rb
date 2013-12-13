e5b21a72834ddc2ebe83673e31557dec5c40f83c
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-12-13 18:46:25 +0000

    email entire consent

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -29,10 +29,15 @@ class ParticipantMailer < ActionMailer::Base
 
   def blood_draw_appointment_email(user)
     require 'prawn'
-    Prawn::Document.generate('tmp/consent.pdf') do
-      text 'You consented!'
+    path = File.join(Rails.root, "tmp/consent-#{ Time.now.to_f }.pdf")
+    ac = ActionController::Base.new
+    body = ac.render_to_string(partial: 'previously_ill_consents/consent', layout: false)
+        .gsub(/<[^>]+>/, '')
+    body << ac.render_to_string(partial: 'previously_ill_consents/responses', layout: false, locals: { consent: user.previously_ill_consent })
+    Prawn::Document.generate(path) do
+      text body, inline_format: true
     end
-    attachments['consent.pdf'] = File.read('tmp/consent.pdf')
+    attachments['consent.pdf'] = File.read(path)
     @user = user
     mail to: @user.email,
          subject: 'Fluomics: Blood draw appointment'
