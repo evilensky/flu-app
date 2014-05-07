dc01226941471c64a996c2e85662b9de66706759
Eugene Vilensky <evilensky@gmail.com>
2014-05-07 17:34:12 -0500

    fix misspelled virtual ohst

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+#role :app, %w{deploy@example.com}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'fluomics.northwestern.edu', user: 'deploy', roles: %w{web db}, primary: true
+server 'cbits-railsapps.nubic.northwestern.edu', user: 'deploy', roles: %w{web db}, primary: true
 
 
 # Custom SSH Options
